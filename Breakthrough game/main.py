cdb1b70f1bd577912ab5f84ebc567a72f0eda74c
Md Istiaq Ahmed <mdistiaqahmed0@gmail.com>
2025-05-08 19:31:41 -0500

    Some works for my ai class

---
+++ Breakthrough game/main.py
@@ -0,0 +1,21 @@
+# Project 2 by Md Istiaq Ahmed & Tashnim Khan Ishika
+
+# run this file to run the game!!!!
+
+from runner import play_game, summarize_result
+from agents import (
+    offensive_heuristic_1,
+    defensive_heuristic_1
+)
+
+# Minimax vs Alpha-beta
+result = play_game(
+    player1_type="minimax",
+    player1_heuristic=offensive_heuristic_1,
+    player1_depth=3,
+    player2_type="alphabeta",
+    player2_heuristic=defensive_heuristic_1,
+    player2_depth=3
+)
+
+summarize_result(result)
